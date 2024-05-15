42c31872b548a807e382c09d7608d9430cfa840b
thobui14ig <buithanhtho14ig@gmail.com>
2024-05-15 22:51:13 +0700

    ok

--- dist/main.js
+++ dist/main.js
@@ -4,11 +4,12 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.setGlobalPrefix('api');
     app.enableCors({
-        origin: ['http://localhost:3000', 'http://localhost:5173'],
+        origin: ['http://localhost:5000'],
         credentials: true,
     });
-    await app.listen(9000);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
