d4497593e25bcb6f4387dcf76ab3e7a743bad70c
thobui14ig <buithanhtho14ig@gmail.com>
2024-05-15 22:43:30 +0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: ['http://localhost:5000'],
     credentials: true,
   });
 
-  await app.listen(9000);
+  await app.listen(3000);
 }
 bootstrap();
