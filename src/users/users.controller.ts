68107e8b0043ee26f59c702f079b76e11645a4a3
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-22 00:16:29 +0700

    ok

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { Public } from 'src/libs/guard/guard';
 
 @Controller('users')
 export class UsersController {
@@ -24,6 +25,7 @@ export class UsersController {
   }
 
   @Get()
+  @Public()
   findAll() {
     return this.usersService.findAll();
   }
