45fbead5d142e05d1e6814bddb9d84d1086fa085
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-30 14:28:32 +0700

    ok

--- src/gateway/services/socket.service.ts
+++ src/gateway/services/socket.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter";
 import { AppGateway, Message } from "../app.gateway";
 import { DistanceService } from "src/distance/distance.service";
-import { calculateDistance, calculateTravelTime, getAddressReceiveAndDeliver } from "src/libs/utils/location";
+import { getAddressReceiveAndDeliver } from "src/libs/utils/location";
 
 @Injectable()
 export class SocketService {
@@ -18,33 +18,25 @@ export class SocketService {
 
         if (!receive) return; 
 
-        if (!deliver){
+        // if (!deliver){
         const locationStart = await this.distanceService.getLocaltionStart(receive)
         payload.locationStart = locationStart
-        } else {
-            console.log(1111, payload.content)
-            const [locationStart, locationEnd] = await Promise.all([
-                this.distanceService.getLocaltionStart(receive),
-                this.distanceService.getLocaltionStart(deliver)
-            ])
-            console.log(22222, locationStart)
-            console.log(33333, locationEnd)
-            console.log('=================')
+        // } else {
+        //     const [locationStart, locationEnd] = await Promise.all([
+        //         this.distanceService.getLocaltionStart(receive),
+        //         this.distanceService.getLocaltionStart(deliver)
+        //     ])
+        //     const distanceAB = calculateDistance(locationStart, locationEnd)
 
-            if ((locationStart && (locationStart.latitude && locationStart.longitude))
-                && (locationEnd && (locationEnd.latitude && locationEnd.longitude))
-            ) {
-                const distanceAB = calculateDistance(locationStart, locationEnd)
-                if (distanceAB) {
-                    payload.distanceAB = distanceAB || null    
-                    const time = calculateTravelTime(distanceAB, 40)
-                    payload.time = time || null      
-                }
-            }
+        //     if (distanceAB) {
+        //         payload.distanceAB = distanceAB || null    
+        //         const time = calculateTravelTime(distanceAB, 40)
+        //         payload.time = time || null      
+        //     }
 
-            payload.locationStart = locationStart
-            payload.locationEnd = locationEnd     
-        }
+        //     payload.locationStart = locationStart
+        //     payload.locationEnd = locationEnd     
+        // }
 
         return this.gateWay.postMessage(payload)
     }
